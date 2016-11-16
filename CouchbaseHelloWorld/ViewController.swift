6653b4f08f54b176063fa6488d82873d488b89d2
LOPEJ520 <John.X.Lopez.-ND@Disney.com>
2016-11-15 23:25:06 -0500

    Initial Commit

---
+++ CouchbaseHelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CouchbaseHelloWorld
+//
+//  Created by Usuario on 11/15/16.
+//  Copyright © 2016 Jhon Jaiver López. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
