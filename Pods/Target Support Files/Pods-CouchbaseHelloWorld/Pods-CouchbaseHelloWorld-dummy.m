86a3cc83194b14dc52b6360af24e1107f9dcf4a7
LOPEJ520 <John.X.Lopez.-ND@Disney.com>
2016-11-18 10:20:18 -0500

    Added Pods folder in order to avoid the making pod install

---
+++ Pods/Target Support Files/Pods-CouchbaseHelloWorld/Pods-CouchbaseHelloWorld-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CouchbaseHelloWorld : NSObject
+@end
+@implementation PodsDummy_Pods_CouchbaseHelloWorld
+@end
---
+++ Pods/Target Support Files/Pods-CouchbaseHelloWorldTests/Pods-CouchbaseHelloWorldTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CouchbaseHelloWorldTests : NSObject
+@end
+@implementation PodsDummy_Pods_CouchbaseHelloWorldTests
+@end
